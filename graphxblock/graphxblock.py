f60800dad4487c86f5468f4822328aa3dd4bfa26
Farhaan Bukhsh <farhaan@opencraft.com>
2025-09-24 16:26:07 +0530

    feat: Adds feature to change line style and add axis labels
    The desmos api is used to change the line style and add axis labels. In addition
    to this we have add the ability to hide the seed equation.
    
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- graphxblock/graphxblock.py
+++ graphxblock/graphxblock.py
@@ -4,16 +4,25 @@ from importlib.resources import files
 
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import String, Scope
+from xblock.fields import Boolean, String, Scope
 from xblock.utils.studio_editable import StudioEditableXBlockMixin
+from xblock.validation import ValidationMessage
 
+def line_style_choice():
+  """ Choices for the line style in graph.
+  """
+  return [
+    { "display_name": "Solid", "value": "Desmos.Styles.SOLIID"},
+    { "display_name": "Dashed", "value": "Desmos.Styles.DASHED"},
+    { "display_name": "Dotted", "value": "Desmos.Styles.DOTTED"},
+  ]
 
 class GraphXBlock(StudioEditableXBlockMixin, XBlock):
     """
     Graph XBlock is used to plot grahs for the student,
     """
     api_key = String(
-        default="Some API Key",
+        default="",
         scope=Scope.content,
         help="Desmos API key to load the graph calculator",
     )
@@ -25,7 +34,38 @@ class GraphXBlock(StudioEditableXBlockMixin, XBlock):
         scope=Scope.content,
     )
 
-    editable_fields = ("display_name", "api_key", "seed_equation")
+    line_style = String(
+        display_name="Line Style",
+        help="The style of lines to show on graph",
+        default="Solid",
+        values = line_style_choice,
+        scope=Scope.content,
+    )
+
+    xaxis_label = String(
+        display_name="X AXIS LABEL",
+        help="The label for X Axis",
+        default="",
+        scope=Scope.content,
+    )
+
+    yaxis_label = String(
+        display_name="Y AXIS LABEL",
+        help="The label for Y Axis",
+        default="",
+        scope=Scope.content,
+    )
+
+    hide_expression = Boolean(
+        display_name="Hide Expression",
+        help="Setting this option to true will make the graph visible but will \
+      hide the expression",
+        default=False,
+        scope=Scope.content,
+    )
+
+    editable_fields = ("display_name", "api_key", "seed_equation", "line_style",
+                       "xaxis_label", "yaxis_label", "hide_expression")
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -42,9 +82,17 @@ class GraphXBlock(StudioEditableXBlockMixin, XBlock):
         frag.add_css(self.resource_string("static/css/graphxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/graphxblock.js"))
         frag.initialize_js('GraphXBlock', {
-            "default_expression": self.seed_equation, 
+            "default_expression": self.seed_equation,
+            "line_style": self.line_style,
+            "x_axis_lable": self.xaxis_label,
+            "y_axis_lable": self.yaxis_label,
+            "hide_expression": self.hide_expression,
         })
         return frag
+
+    def validate_field_data(self, validation, data):
+      if len(data.api_key.strip()) == 0:
+        return validation.add(ValidationMessage(ValidationMessage.ERROR, "Please add an API Key"))
 
     @staticmethod
     def workbench_scenarios():
--- graphxblock/static/js/src/graphxblock.js
+++ graphxblock/static/js/src/graphxblock.js
@@ -4,7 +4,22 @@ function GraphXBlock(runtime, element, data) {
   let graphBlock = $('#graphxblock_block', element).get(0);
   let graphCalculator = Desmos.GraphingCalculator(graphBlock);
   let default_expression = data?.default_expression;
+  let lineStyle = data?.line_style;
+  let xAxisLabel = data?.x_axis_lable;
+  let yAxisLabel = data?.y_axis_lable;
+  let hideExpression = data?.hide_expression;
   if (default_expression !== "") {
-    graphCalculator.setExpression({ latex: default_expression });
+    graphCalculator.setExpression({
+      latex: default_expression,
+      lineStyle: eval(lineStyle),
+      secret: hideExpression,
+    });
   }
+  if (xAxisLabel !== "") {
+    graphCalculator.updateSettings({ xAxisLabel: xAxisLabel });
+  }
+  if (yAxisLabel !== "") {
+    graphCalculator.updateSettings({ yAxisLabel: yAxisLabel });
+  }
+
 }
