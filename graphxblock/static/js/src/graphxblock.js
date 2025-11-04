8d6774359d2e31fcc6d2ba2949723648b9fc3a54
Farhaan Bukhsh <farhaan@opencraft.com>
2025-11-04 16:47:37 +0530

    feat: Add state saving in the graph
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- graphxblock/static/js/src/graphxblock.js
+++ graphxblock/static/js/src/graphxblock.js
@@ -4,10 +4,14 @@ function GraphXBlock(runtime, element, data) {
   let graphBlock = $('#graphxblock_block', element).get(0);
   let graphCalculator = Desmos.GraphingCalculator(graphBlock);
   let default_expression = data?.default_expression;
-  let lineStyle = data?.line_style;
+  debugger;
+  let lineStyle = data?.line_style === "" ? Desmos.Styles.SOLIID : data?.line_style;
   let xAxisLabel = data?.x_axis_lable;
   let yAxisLabel = data?.y_axis_lable;
   let hideExpression = data?.hide_expression;
+  let saveState = data?.save_state_allowed;
+  let state = data?.state;
+  let saveStateUrl = runtime.handlerUrl(element, 'save_state');
   if (default_expression !== "") {
     graphCalculator.setExpression({
       latex: default_expression,
@@ -21,5 +25,28 @@ function GraphXBlock(runtime, element, data) {
   if (yAxisLabel !== "") {
     graphCalculator.updateSettings({ yAxisLabel: yAxisLabel });
   }
+
+  if ((state !== undefined && state !== "") && saveState) {
+    graphCalculator.setState(state);
+  }
+
+  let stateButton = $('#stateButton', element).get(0);
+  if (saveState) {
+    stateButton.style.visibility = 'visible';
+  } else {
+    stateButton.style.visibility = 'hidden';
+  }
+
+  stateButton.addEventListener('click', () => {
+    let currentState = graphCalculator.getState();
+    $.ajax({
+      type: 'POST',
+      url: saveStateUrl,
+      data: JSON.stringify(currentState),
+      dataType: 'json',
+      success: () => { alert('State saved!') },
+    })
+
+  })
 
 }
