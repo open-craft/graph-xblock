51033e2adfd6580595ba59d3ec9897a9f766f254
Farhaan Bukhsh <farhaan@opencraft.com>
2025-11-19 16:16:57 +0530

    feat: Adds inline status message for the save state request.
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- graphxblock/static/js/src/graphxblock.js
+++ graphxblock/static/js/src/graphxblock.js
@@ -4,6 +4,9 @@ function GraphXBlock(runtime, element, data) {
   // Don't change this to jquery find format because the render and find
   // doesn't play with Desmos.
   let graphBlock = $('#graphxblock_block', element).get(0);
+  let statusBanner = $(element).find('#statusBanner');
+  let closeBannerButton = $(element).find('#closeBannerButton');
+  let statusMessage = $(element).find('#statusMessage');
   let graphCalculator = Desmos.GraphingCalculator(graphBlock);
   let default_expression = data?.default_expression;
   let lineStyle = data?.line_style === "" ? Desmos.Styles.SOLIID : data?.line_style;
@@ -31,6 +34,12 @@ function GraphXBlock(runtime, element, data) {
     graphCalculator.setState(state);
   }
 
+  // Function to close the status banner.
+  closeBannerButton.on('click', () => {
+    statusMessage.text('');
+    statusBanner.css('display', 'none');
+  })
+
   /* Create button on the go, it checks if the save state option is enabled
    * and only if the option is enabled we create the button and add it to the div.
    * */
@@ -44,7 +53,16 @@ function GraphXBlock(runtime, element, data) {
         url: saveStateUrl,
         data: JSON.stringify(currentState),
         dataType: 'json',
-        success: () => { alert('State saved!') },
+        success: () => {
+          statusMessage.text('Your graph is succuessfully saved!');
+          statusBanner.css('display', 'flex');
+        },
+        error: (xhr, textStatus, errorThrown) => {
+          let message = `The request failed with status code: ${xhr.status} and message: ${textStatus}`;
+          statusMessage.text(message);
+          statusBanner.css('background-color', '#f44336');
+          statusBanner.css('display', 'flex');
+        }
       })
     })
     parentButton.append(saveButton)
